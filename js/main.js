67ddc61bf3c28469ebe929fc97c3d443e484868e
Shanna Ginos <Shanna@Dustins-MacBook-Pro.local>
2021-03-03 07:51:10 -0700

    added localstorage functionality

--- js/main.js
+++ js/main.js
@@ -1,16 +1,15 @@
 const bidsArray = [];
-let myStorage = window.localStorage;
-localStorage.setItem('all-bids', JSON.stringify(bidsArray));
-let storedBids = JSON.parse(localStorage.getItem("all-bids"));
 
 document.getElementById('place-bid1').addEventListener('click', function(){pushBid('bid-text1')});
 document.getElementById('place-bid2').addEventListener('click', function(){pushBid('bid-text2')});
+document.getElementById('local-storage-btn').addEventListener('click', getFromLocal);
 
 function pushBid(id) {
     const bid = document.getElementById(id).value;
     bidsArray.push(bid);
     document.getElementById(id).value = '';
     printBids();
+    saveToLocal();
 };
 
 function printBids() {
@@ -18,3 +17,15 @@ function printBids() {
         <h3>${bidsArray}</h3>
     `;
 };
+
+function saveToLocal(){
+    localStorage.setItem('all-bids', JSON.stringify(bidsArray));
+    document.getElementById('alert-box').innerText = "Bid saved to local storage."
+}
+
+function getFromLocal(){
+    let storedBids = localStorage.getItem("all-bids");
+    let bidsArray2 = JSON.parse(storedBids);
+    document.getElementById('bid-array').innerText = bidsArray2;
+    document.getElementById('alert-box').innerText = "Bids have been retrieved from local storage." 
+}
